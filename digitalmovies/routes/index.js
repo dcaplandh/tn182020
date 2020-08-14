42caa0c3a302f1aec3047755654600ee5b4d8823
diego <dcaplan@digitalhouse.com>
2020-08-13 21:20:19 -0300

    html css clase 1

--- digitalmovies/routes/index.js
+++ digitalmovies/routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Diego Caplan' });
 });
 
+router.get("/clase1",function(req,res){
+  res.render("clase1");
+});
+
+router.get("/mercadoliebre",function(req,res){
+  res.render("meli");
+});
+
 module.exports = router;
